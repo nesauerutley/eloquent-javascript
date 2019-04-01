bcf841bdc928c4328a656fd20955f7da340a138e
nesauerutley <naomisauerutley@gmail.com>
2019-04-01 12:30:25 -0400

    Add exercise 4.4

---
+++ eloquentJavascript4_4.js
@@ -0,0 +1,36 @@
+/*
+Deep comparison
+The == operator compares objects by identity. But sometimes you’d prefer to compare the values of their actual properties.
+
+Write a function deepEqual that takes two values and returns true only if they are the same value or are objects with the same properties, where the values of the properties are equal when compared with a recursive call to deepEqual.
+
+To find out whether values should be compared directly (use the === operator for that) or have their properties compared, you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. But you have to take one silly exception into account: because of a historical accident, typeof null also produces "object".
+
+The Object.keys function will be useful when you need to go over the properties of objects to compare them.
+*/
+
+function deepEqual(x, y) {
+  if (x===y) return true;
+
+  if ((typeof x == "object" && x != null) && (typeof y == "object" && y != null)) {
+    if (Object.keys(x).length != Object.keys(y).length)
+      return false;
+  }
+  let xKeys = Object.keys(x), yKeys = Object.keys(y);
+
+  if (xKeys.length != yKeys.length) return false;
+
+  for (let key of xKeys) {
+    if (!yKeys.includes(key) || !deepEqual(x[key], y[key])) return false;
+  }
+
+  return true;
+}
+
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
